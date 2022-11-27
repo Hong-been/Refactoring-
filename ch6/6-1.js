033d6bd4be7fa35d7fd09b1af3fae53cb0f127cd
HongBeen <redbean-2@naver.com>
2022-11-27 17:45:11 +0900

    Extract function

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,31 +1,40 @@
 export function printOwing(invoice) {
-  let outstanding = 0;
-
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
-
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
-
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+	printBanners();
+	let outstanding = calculateOutstanding(invoice);
+	recordDueDate(invoice);
+	printDetails(invoice, outstanding);
 }
 
 const invoice = {
-  orders: [{ amount: 2 }, { amount: 5 }],
-  customer: '엘리',
+	orders: [{amount: 2}, {amount: 5}],
+	customer: "엘리",
 };
+
+function printBanners() {
+	console.log("***********************");
+	console.log("**** Customer Owes ****");
+	console.log("***********************");
+}
+
+function calculateOutstanding(invoice) {
+	return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
+
+function recordDueDate(invoice) {
+	const today = new Date();
+
+	// TODO: 객체 불변성은 나중 챕터에서 심오하게 다뤄볼 예정이에요
+	invoice.dueDate = new Date(
+		today.getFullYear(),
+		today.getMonth(),
+		today.getDate() + 30
+	);
+}
+
+function printDetails(invoice, outstanding) {
+	console.log(`name: ${invoice.customer}`);
+	console.log(`amount: ${outstanding}`);
+	console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
 printOwing(invoice);
