576e4ba2a0cba7ba7645b601829d6cf80dad1cf7
HongBeen <redbean-2@naver.com>
2023-01-03 00:25:37 +0900

    record, collection capsulated

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,25 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+const organization = new Organization("Acme Gooseberries", "GB");
 
-organization.name = 'Dream Coding';
+organization.name = "Dream Coding";
 console.log(organization.name);
 console.log(organization.country);
+
+class Organization {
+	#name;
+	#country;
+	constructor(data) {
+		this.#name = data.name;
+		this.#country = data.country;
+	}
+
+	get name() {
+		return this.#name;
+	}
+	get country() {
+		return this.#country;
+	}
+
+	set name(name) {
+		return (this.#name = name);
+	}
+}
